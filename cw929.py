67e617b4a3b3175284b57cefd9c9d7d7550ab978
Kevin Li <kevinli@dwightshrute.local>
2017-10-06 14:32:40 -0400

    update

--- cw929.py
+++ cw929.py
@@ -1,19 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session, redirect
 
 cw929 = Flask(__name__)
+cw929.secret_key = 'random'
 
 @cw929.route('/')
 def root():
-  return render_template("form.html")
+    if 'name' in session:
+        #print "test"
+        return render_template("response.html", name = session['name'], method = request.method)
+    else:
+        return render_template("form.html")
+
+name = 'default'
+password = 'password'
 
 @cw929.route('/response', methods=['POST','GET'])
 def response():
-    name = 'default'
     if request.method == 'POST':
         name = request.form['username']
+        password = request.form['password']
     else:
         name = request.args['username']
-    return render_template("response.html", input = name, method = request.method)
+        password = request.args['password']
+    if (name == 'kevincyn' and password == 'secretpassword'):
+        session['name'] = name
+        return render_template("response.html", name = session['name'], method = request.method, password = password)
+    else:
+        if (name != 'kevincyn'):
+            problem = 'username'
+        else:
+            problem = 'password'
+        return render_template("loginfailed.html", problem = problem)
+
+@cw929.route('/logout', methods=['POST','GET'])
+def logout():
+    if 'name' in session:
+        session.pop('name')
+        #print "testingg"
+    return redirect("/")
 
 if __name__ == '__main__':
   cw929.debug = True
